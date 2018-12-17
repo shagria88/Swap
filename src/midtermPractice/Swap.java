46aab21ee95f26d0ffbad872824f5e978ff3330f
shagria kabir <shagria.kabir@gmail.com>
2018-12-17 13:45:01 -0500

    1st commit

---
+++ src/midtermPractice/Swap.java
@@ -0,0 +1,25 @@
+package swapPack;
+
+public class Swap2 {
+    public static void main(String[] args) {
+        int value1 = 10;
+        int value2 = 20;
+
+        //without swapping
+        System.out.println("Before swapping: ");
+        System.out.println(value1);
+        System.out.println(value2);
+
+
+        //with Swapping
+        int temp = value1;
+        value1 = value2;
+        value2 = temp;
+
+        System.out.println("After swapping ");
+        System.out.println(value1);
+        System.out.println(value2);
+
+    }
+
+}
